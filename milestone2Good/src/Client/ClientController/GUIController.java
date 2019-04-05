3cb34659c23ec080ca8ec09a02600b3daf1fa7de
Harsohail Brar <harsohailbrar@yahoo.ca>
2019-04-05 12:32:59 -0600

    changed folder locations

---
+++ milestone2Good/src/Client/ClientController/GUIController.java
@@ -0,0 +1,33 @@
+package Client.ClientController;
+
+/**
+ * This class is an abstract class with acts as the super class for all GUI
+ * Controllers
+ */
+public abstract class GUIController {
+
+    /**
+     * Client Controller consisting of sockets
+     */
+    protected ClientController clientController;
+
+    /**
+     * Constructor for the class
+     * @param c Client Controller
+     */
+    public GUIController(ClientController c){
+        setClientController(c);
+    }
+
+    /**
+     * Client controller setter which creates a
+     * 2-way association between the classes
+     * @param c client controller
+     */
+    public void setClientController(ClientController c){
+        clientController = c;   // 2-way association with CC
+    }
+
+
+
+}
---
+++ milestone2Good/src/Client/ClientController/LoginController.java
@@ -0,0 +1,89 @@
+package Client.ClientController;
+
+import Client.ClientView.LoginView;
+//import Server.ServerModel.*;
+import utils.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * This class is responsible for controlling the login view
+ * as well as creating the main window after login
+ *
+ * @author  Gary Wu, Harsohail Brar, Ryan Holt
+ * @version 4.10.0
+ * @since April 5, 2019
+ */
+public class LoginController extends GUIController{
+
+    //MEMBER VARIABLES
+
+    private LoginView loginView;
+    private boolean verified;
+
+    /**
+     * Creates a LoginController object and adds the object listener for the
+     * log in button
+     * @param m main GUI controller object
+     * @param l login view object
+     * @param o output socket stream
+     * @param i input socket stream
+     */
+    public LoginController(LoginView l, ClientController cc){
+        super(cc);
+        loginView = l;
+        verified = false;
+
+        loginView.addLoginListener(new LoginListen());
+    }
+
+    /**
+     * Action Listener implementation for Login Button
+     */
+    class LoginListen implements ActionListener{
+
+        /**
+         * When the user clicks on the button it checks to see if
+         * the username and password is the same as one of our users
+         * from our database and if yes the GUI becomes visible else
+         * a box will prompt telling user is invalid
+         */
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource() == loginView.getLoginButton()){
+                try{
+                    String username = loginView.getUsernameField().getText();
+                    String password = loginView.getPasswordFeild().getText();
+
+                    clientController.getSocketOut().writeObject(new User(username, password));
+
+                    String verification = (String)clientController.getSocketIn().readObject();
+
+                    if(verification.equals("verified")) {
+                        loginView.setVisible(false);
+                        verified = true;
+                        System.out.println("User Logged In!");
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Invalid User!");
+                    }
+
+                    clientController.getSocketOut().flush();
+                }catch(Exception f){
+                    f.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    //getters and setters
+    public LoginView getLoginView() {
+        return loginView;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+}
